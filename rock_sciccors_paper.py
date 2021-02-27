4bbb6e02f655f78f7d5e237678c4ab0273ecfdfd
Arda Cetinkaya <ardacetinkaya@gmail.com>
2021-02-27 14:09:30 +0300

    Initial

---
+++ rock_sciccors_paper.py
@@ -0,0 +1,74 @@
+from time import sleep
+from sense_hat import SenseHat
+import random
+
+e = (0, 0, 0)
+w = (255, 255, 255)
+r = (255, 0, 0)
+y = (255, 255, 0)
+
+scissors = [
+   y, y, y, y, y, y, r, y,
+   r, r, y, y, y, r, y, y,
+   r, r, y, y, r, y, y, y,
+   y, y, r, r, y, y, y, y,
+   y, y, r, r, y, y, y, y,
+   r, r, y, y, r, y, y, y,
+   r, r, y, y, y, r, y, y,
+   y, y, y, y, y, y, r, y
+]
+
+paper = [
+   y, y, y, y, y, y, y, y,
+   y, y, y, y, y, y, y, y,
+   y, y, y, y, y, y, y, y,
+   y, y, y, y, y, y, y, y,
+   y, r, r, r, r, r, r, y,
+   y, y, y, y, y, y, y, y,
+   y, y, y, y, y, y, y, y,
+   y, y, y, y, y, y, y, y,
+]
+
+rock = [
+   y, y, y, y, y, y, y, y,
+   y, y, y, r, y, y, y, y,
+   y, y, r, r, r, r, y, y,
+   y, y, r, r, r, r, r, y,
+   y, r, r, r, r, r, y, y,
+   y, r, r, r, r, r, y, y,
+   y, y, r, r, r, y, y, y,
+   y, y, y, y, y, y, y, y,
+]
+
+
+options =dict([('paper',paper), ('scissors',scissors), ('rock', rock)])
+hat = SenseHat()
+
+def startCountDown(f):
+    for i in range(f,0,-1):
+      print(str(i))
+      hat.show_letter(str(i),r)
+      sleep(0.5)
+    print('GO!!!')
+
+def playRockSciccorsPaper(times):
+  for i in range(times,0,-1):
+    sleep(0.5)
+    hat.clear()
+    startCountDown(3)
+    key, value = random.choice(options.items())
+    print(key)
+    hat.set_pixels(value)
+    sleep(0.75)
+
+hat.clear()
+while True:
+  for event in hat.stick.get_events():
+    if event.action == "pressed":
+      if event.direction == "up":
+        print('Let''s play Rock-Scissors-Paper')
+        playRockSciccorsPaper(3)
+        hat.clear()
+        print('This rount is completed. Press up to play again.')
+
+
